f26f15754076455c1a4979aa014ec98ff7da564d
Lukas <lukasabbehb@gmail.com>
2025-05-07 11:51:44 +0200

    Added more stats

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const nj = require("nunjucks");
 (async()=>{
     const github_response = await fetch("https://api.github.com/users/lukasabbe/repos");
     let total_stars = 0;
+    let fork_amount = 0;
 
     const github_json_data = await github_response.json();
 
     github_json_data.forEach(element => {
         total_stars += element.stargazers_count;
+        fork_amount += element.forks;
     });
 
-    const rendered = nj.render("README_TEMPLATE.njk", {stars: total_stars});
+    const rendered = nj.render("README_TEMPLATE.njk", {stars: total_stars, projects: github_json_data.length, forks: fork_amount});
     fs.writeFileSync("./README.md",rendered);
 })()
